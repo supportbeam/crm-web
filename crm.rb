3ebe084ba861ba47324da16bb20ce524c05e37f6
JJLim <lim.jess9@gmail.com>
2015-01-27 14:40:01 -0500

    Created an instance variable to pass into view in index.erb

--- crm.rb
+++ crm.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 
 # Route to request and respond with Main Menu
 get '/' do
+	@crm_app_name = "My CRM" #set up instance variable
   erb :index #view index in embeddable ruby
 end
 
